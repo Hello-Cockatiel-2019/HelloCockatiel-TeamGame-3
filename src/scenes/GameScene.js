da498069b1984b92601eb5e4d7c578cb58bc795e
pattarapol <pattarapol.plasma@mail.kmutt.ac.th>
2019-11-02 15:17:15 +0700

    [UPDATE]Movement_Player

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -11,37 +11,38 @@ class GameScene extends Phaser.Scene{
     }
 
     preload(){
-            this.load.image('sky','images/sky.png');
-            this.load.image('glass','images/glass.png');
             this.load.image('player','../images/Gokuตัดเองจ้า.png')
     }
     
     create(){
-            background = this.add.image(0,0,'sky').setOrigin(0).setScale(3)
-            platforms = this.physics.add.staticGroup();
+        
         player = this.physics.add.image(384, 216, 'player').setSize(400, 300).setScale(0.2).setCollideWorldBounds(true)
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
-            platforms.create(300, 750, 'glass').setScale(3).refreshBody();
     }
 
     update(){
-        bg.tilePositionX -= 1
-        {
-            if (keyA.isDown) {
-                player.setVelocityX(-500)
-            } else if (keyD.isDown) {
-                player.setVelocityX(500)
-            } else if (keyS.isDown) {
-                player.setVelocityY(+330)
-            } if (keyW.isDown) {
-                player.setVelocityY(-330)
-            }
+    {
+        if (keyA.isDown) {
+            player.setVelocityX(-400)
+
+        } else if (keyD.isDown) {
+            player.setVelocityX(400)
+
+        } else  {
+            player.setVelocityX(0)
+        }
+        if (keyW.isDown) {
+            player.setVelocityY(-400)
+        }else if (keyS.isDown) {
+            player.setVelocityY(400)
+        }else  {
+            player.setVelocityY(0)
         }
     }
-
+    }
 }
 export default GameScene
