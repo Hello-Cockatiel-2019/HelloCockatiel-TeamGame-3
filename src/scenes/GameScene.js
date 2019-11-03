4bb364a5ec41d2e59bc7c99646be57b025145583
JJ <yinyin.domo@hotmail.com>
2019-11-03 22:49:16 +0700

    [UPDATE] แก้ภาพ BG

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-import { platform } from "os"
-
-=======
->>>>>>> Dlz
 var background
 var platforms
 let keyW,keyA,keyS,keyD
 let player
+let tree
 class GameScene extends Phaser.Scene{
 
     constructor(){
@@ -16,8 +12,8 @@ class GameScene extends Phaser.Scene{
     }
 
     preload(){
-<<<<<<< HEAD
-            this.load.image('sky','images/sky.png');
+            this.load.image('tree','images/tree.jpg')
+            this.load.image('sky','images/sky.jpg')
             this.load.image('platform','images/platform.png')
             this.load.image('Player','../../images/Gokuตัดเองจ้า.png',{frameWidth :384 ,frameHeight:216})
     }
@@ -25,20 +21,13 @@ class GameScene extends Phaser.Scene{
     create(){
         platforms = this.physics.add.image(0,0,'platform').setOrigin(0.5).setScale(1).setSize(1800,250).setCollideWorldBounds(true);
         background = this.add.image(0,0,'sky').setOrigin(0).setScale(1)
-        player = this.physics.add.image(300,800,'Player').setSize(800,600).setScale(0.2).setCollideWorldBounds(true);
-=======
-            this.load.image('player','../images/Gokuตัดเองจ้า.png')
-    }
-    
-    create(){
+        
         
         player = this.physics.add.image(384, 216, 'player').setSize(400, 300).setScale(0.2).setCollideWorldBounds(true)
->>>>>>> Dlz
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-<<<<<<< HEAD
         this.physics.add.collider(player,platforms);
     }
 
@@ -53,12 +42,6 @@ class GameScene extends Phaser.Scene{
             } if (keyW.isDown) {
                 player.setVelocityY(-330)
             }
-=======
-
-    }
-
-    update(){
-    {
         if (keyA.isDown) {
             player.setVelocityX(-400)
 
@@ -74,7 +57,6 @@ class GameScene extends Phaser.Scene{
             player.setVelocityY(400)
         }else  {
             player.setVelocityY(0)
->>>>>>> Dlz
         }
     }
     }
