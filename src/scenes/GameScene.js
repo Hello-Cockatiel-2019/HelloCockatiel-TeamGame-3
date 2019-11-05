2a9c488baf9208b971a438992f6d3488c8ebfd39
Jednuth <jednuth.tatirat@mail.kmutt.ac.th>
2019-11-05 12:11:50 +0700

    [UPDATE] Set Thunder

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -22,23 +22,27 @@ class GameScene extends Phaser.Scene{
             this.load.image('platform','images/platform.png')
             this.load.image('Player','../../images/Gokuตัดเองจ้า.png',{frameWidth :384 ,frameHeight:216})
             this.load.image('Lightning','../../images/lightning.png')
+            this.load.image('Thunder','../../images/thunder.png')
 
     }
     
     create(){
         platforms = this.physics.add.image(0,0,'platform').setOrigin(0.5).setScale(1).setSize(1800,250).setCollideWorldBounds(true);
         background = this.add.image(0,0,'sky').setOrigin(0).setScale(1)
+        thunder = this.add.image(250,-50,'Thunder').setOrigin(0).setScale(0.5)
+        thunder = this.add.image(10,-50,'Thunder').setOrigin(0).setScale(0.5)
+        thunder = this.add.image(450,-50,'Thunder').setOrigin(0).setScale(0.5)
+        thunder = this.add.image(650,-50,'Thunder').setOrigin(0).setScale(0.5)
         player = this.physics.add.image(384,216,'Player').setSize(800,600).setScale(0.2).setCollideWorldBounds(true);
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         this.physics.add.collider(player,platforms);
-        
         lightningGroup = this.physics.add.group();
         this.physics.add.collider(player,lightningGroup,function(){console.log('test')})
         event = this.time.addEvent({
-            delay : 1500,
+            delay : 3000,
             callback : function(){
                 lightning = this.physics.add.sprite(Phaser.Math.Between(0,900),20,'Lightning').setScale(0.1)
                 lightningGroup.add(lightning)
@@ -46,7 +50,8 @@ class GameScene extends Phaser.Scene{
             },
             callbackScope : this,
             loop : true,
-            pause : true,
+            pause : true,     
+            repeat : 0,                                        
             
             
         })
@@ -67,7 +72,7 @@ class GameScene extends Phaser.Scene{
         }
         for(let i = 0;i < lightningGroup.getLength();i++){
             let thun = lightningGroup.getChildren()[i]
-            if(lightning.y <= -50){
+            if(lightning.y <= -30){
                 lightning.destroy(true)
             }
         }
